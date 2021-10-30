d6e5134ad96e6183b55418cf9186b2455a2a63c0
Suto, Masanori <suto@geek-masa.com>
2021-10-30 11:27:28 +0900

    Improve keymap definition
    Explicitly type myKeys
    Use `addMyKeys' to view all keybindings in xmessage

--- my-xmonad/Main.hs
+++ my-xmonad/Main.hs
@@ -1,12 +1,16 @@
 import XMonad
+import XMonad.Util.EZConfig (checkKeymap)
 
 import XMonadConfig.XConfig (myTerminal, myWorkspaces, myBorderWidth)
-import XMonadConfig.Input    (myModMask, myKeys)
+import XMonadConfig.Input   (myModMask, addMyKeys)
+
+
 
 main :: IO ()
-main = xmonad def
+main = xmonad $ myConfig
+
+myConfig = addMyKeys def
   { modMask     = myModMask
-  , keys        = myKeys
   , terminal    = myTerminal
   , borderWidth = myBorderWidth
   }
--- my-xmonad/XMonadConfig/Input.hs
+++ my-xmonad/XMonadConfig/Input.hs
@@ -1,16 +1,36 @@
 module XMonadConfig.Input
   ( myModMask
-  , myKeys) where
+  , addMyKeys) where
 
 import System.Exit (exitSuccess)
 import XMonad
-import XMonad.Util.EZConfig (mkKeymap)
+
+import qualified Data.Map        as M
+import qualified XMonad.StackSet as W
+import XMonad.Actions.PhysicalScreens (viewScreen, sendToScreen)
+import XMonad.Util.NamedActions (addName, NamedAction, (^++^), HasName, xMessage, addDescrKeys')
+import XMonad.Util.EZConfig (mkKeymap, mkNamedKeymap)
+import XMonad.Util.Run      (safeSpawn, safeSpawnProg)
 
 myModMask :: KeyMask
 myModMask = mod4Mask
 
-myKeys = \c -> mkKeymap c
+addMyKeys :: XConfig l -> XConfig l
+addMyKeys = addDescrKeys' ((myModMask, xK_h), xMessage) myKeys
+
+myKeys :: XConfig Layout -> [((KeyMask, KeySym), NamedAction)]
+myKeys = \c -> mkNamedKeymap c $
   [ -- Terminal and Emacs
-    ("M-v", spawn $ XMonad.terminal c)
-  , ("M-m q", io exitSuccess)
+    ("M-v"       , addName "Open Terminal" $ safeSpawnProg $ XMonad.terminal c)
+  , ("M-e"       , addName "Open Emacs" $ safeSpawn "emacsclient" ["--create-frame"])
+  , ("M-<Tab>"   , addName "Focus Next Window" $ windows W.focusDown)
+  , ("M-S-<Tab>" , addName "Focus Previous Window" $ windows W.focusUp)
+  , ("M-m r"     , addName "Restart XMonad" $ spawn "xmonad --restart")
+  , ("M-m q"     , addName "Quit XMonad" $ io exitSuccess)
+  ]
+  ^++^
+  [
+   (("M-" ++ otherModMasks ++ key), addName "Screen" $ f sc)
+      | (key, sc)          <- zip ["a", "r", "s"] [0..]
+      , (otherModMasks, f) <- [("", viewScreen def), ("S-", sendToScreen def)]
   ]
