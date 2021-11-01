7508bd4134e9a0ae87f11d9727f25762d357af3a
Suto, Masanori <suto@geek-masa.com>
2021-11-01 18:08:07 +0900

    Respond to EWMH Desktop messages

--- my-xmonad/Main.hs
+++ my-xmonad/Main.hs
@@ -1,5 +1,6 @@
 import XMonad
 import XMonad.Util.EZConfig (checkKeymap)
+import XMonad.Hooks.EwmhDesktops (ewmhFullscreen, ewmh)
 
 import XMonadConfig.XConfig (myTerminal, myWorkspaces,
                              myBorderWidth, myFocusFollowsMouse,
@@ -7,7 +8,7 @@ import XMonadConfig.XConfig (myTerminal, myWorkspaces,
 import XMonadConfig.Input   (myModMask, addMyKeys)
 
 main :: IO ()
-main = xmonad $ myConfig
+main = xmonad $ ewmhFullscreen . ewmh $ myConfig
 
 myConfig = addMyKeys def
   { modMask     = myModMask
